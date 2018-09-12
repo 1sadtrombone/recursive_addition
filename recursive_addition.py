8d3ef4349e1ddc0bb10765f76532292fa30ce77b
1sadtrombone <taj.dyson@gmail.com>
2018-09-11 21:41:40 -0400

    Final Version
    All variables are now passed as args, no globals (yay).
    carry is now an int since having it as a list was a little unnecessary.

--- recursive_addition.py
+++ recursive_addition.py
@@ -1,23 +1,19 @@
-A = [0, 0, 3, 1] #1300 (easier to do in reverse, ie lowest value to highest)
-B = [5, 6, 7] #765
-R = [] #result
-c = [0] #carry
-
-def add(R, c):
+def add(A, B, R = [], c = 0): #args, result, carry
 	i = len(R) #operating on this row. 
-	if i == len(A):
-		if c[i] > 0:
-			R.append(c[i]) #make sure we get any carries from the last run
+	
+	if i == 0: #1st run
+	#padding numbers if unequal numbers 
+		if len(A) < len(B): 
+			A += [0] * (len(B) - len(A)) #add a number of 0s equal to the discrepancy in lengths (adds to the right, ie doesn't change value)
+		elif len(B) < len(A):
+			B += [0] * (len(A) - len(B))
+
+	if i == len(A): #gone through all digits
+		if c > 0: R.append(c) #make sure we get the carry from the last run, if any
 		return R #finally return value 
-	else:
-		R.append((A[i]+B[i]+c[i])%10) #next digit 
-		c.append((A[i]+B[i]+c[i])/10) #next carry
-		return add(R, c) #is this really recursion? I feel like I could write it out w/o it, and R, c doesn't really need to be passed, they're global.
+	else: 
+		R.append((A[i]+B[i]+c)%10) #next digit 
+		c = (A[i]+B[i]+c)/10 #next carry
+		return add(A, B, R, c) #go onto the next iteration, the next digit
 
-#padding numbers if unequal number of digits
-if len(A) < len(B): 
-	A += [0] * (len(B) - len(A)) #add a number of 0s equal to the discrepancy in lengths (adds to the right, ie doesn't change value)
-elif len(B) < len(A):
-	B += [0] * (len(A) - len(B))
-
-print list(reversed(add(R, c))) #prints result in forward order
+print list(reversed(add([0, 0, 3, 1], [5, 6, 7]))) #inputs in reverse digit order, prints result in forward order
